2cab1544c9e0c7cb8e9e62de22d368264f303408
soliyyev_ <soliyevabdurauf63@gmail.com>
2025-02-10 20:52:57 +0500

    vazifalar joylandi

---
+++ task.js
@@ -0,0 +1,30 @@
+// Qo'shimcha vazifalar
+
+/** EXTRA 1
+gender nomli o‘zgaruvchiga "male" yoki "female"  qiymatlarini tayinlash uchun ternary operator dan foydalaning.
+Tanlov isMale nomli boshqa o‘zgaruvchining qiymatiga asoslangan bo‘lishi kerak. Ya'ni agar IsMale true bo'lsa bizga male textini qaytarsin,
+aks holda female textini qaytarsin.
+*/
+
+/** EXTRA 2
+Ikki butun son berilgan taqdirda, quyidagi shartlarni tekshirish uchun kod yozing:
+
+Ulardan biri 8 ga tengmi?
+Yoki ularning yig‘indisi yoki ayirmasi 8 ga tengmi? Agar teng bo‘lsa, true qiymatini qaytaring, aks holda false.
+*/
+
+/** EXTRA 3
+O‘zgaruvchi yarating va unga ikki (string) birikmasini (concatenation) tayinlang.
+*/
+
+/** EXTRA 4
+Ikki berilgan butun sonning o‘rtacha qiymatini topish uchun kod yozing.
+*/
+
+/** EXTRA 5
+Ikki berilgan stringni eng uzunini topish uchun kod yozing.
+*/
+
+/** EXTRA 6
+Berilgan qiymat butun son (integer) ekanligini tekshirish uchun kod yozing.
+*/
